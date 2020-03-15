f39db1ff9bafc83db6e44a6434bdba13ee47f541
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-15 14:36:00 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,19 +1,24 @@
 require 'nokogiri'
 require 'open-uri'
+require'pry'
+
 
 require_relative './course.rb'
 
 class Scraper
+  def get_page
+
+  end
   
-  def print_courses
-    self.make_courses
-    Course.all.each do |course|
-      if course.title && course.title != ""
-        puts "Title: #{course.title}"
-        puts "  Schedule: #{course.schedule}"
-        puts "  Description: #{course.description}"
-      end
-    end
+  # def print_courses
+  #   self.make_courses
+  #   Course.all.each do |course|
+  #     if course.title && course.title != ""
+  #       puts "Title: #{course.title}"
+  #       puts "  Schedule: #{course.schedule}"
+  #       puts "  Description: #{course.description}"
+  #     end
+  #   end
   end
   
 end
