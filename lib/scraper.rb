e456c5a380a99ed20e95e6a3316179f4b81e70d0
Michael Kwiatkowsky <mikeski885@gmail.com>
2020-03-15 15:02:03 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -7,9 +7,10 @@ require_relative './course.rb'
 
 class Scraper
   def get_page
-    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses")
+    doc = Nokogiri::HTML(open("http://learn-co-
+    curriculum.github.io/site-for-scraping/courses")
 
-    binding.pry
+        binding.pry
   end
 end
 Scraper.new.get_page
